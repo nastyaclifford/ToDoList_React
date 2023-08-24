d8dea6ad46beb95d770c36f6f6a216cd9978fb72
nastyaclifford <madchen95@mail.ru>
2023-08-23 21:28:40 -0600

    created simple todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TodoList from "./TodoList";
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, title: "First todo", completed: false },
+    { id: 2, title: "Second todo", completed: false },
+  ]);
+
+  const [todoTitle, setTodoTitle] = useState("");
+
+  const addTodo = (event) => {
+    if (event.key === "Enter") {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title: todoTitle,
+          completed: false,
+        },
+      ]);
+      setTodoTitle("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="label_box">
+        <h1>Todo app</h1>
+
+        <div className="input-field">
+          <input
+            type="text"
+            value={todoTitle}
+            onChange={(event) => setTodoTitle(event.target.value)}
+            onKeyPress={addTodo}
+          />
+          <label className="input_label">Todo name</label>
+        </div>
+      </div>
+
+      <TodoList todos={todos} />
     </div>
   );
 }
-
-export default App;
